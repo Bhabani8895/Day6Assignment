061a163e41f46ac88260a7929fa3a3f30912c25c
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2021-11-30 08:36:44 +0530

    program added to branch3

--- Day6Assignment/PrimeNumber.cs
+++ Day6Assignment/PrimeNumber.cs
@@ -8,5 +8,21 @@ namespace Day6Assignment
 {
     internal class PrimeNumber
     {
+        public void prime()
+        {
+            Console.WriteLine("Enter any number to check whether it prime or not!!!");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    count++;
+
+            }
+            if (count == 2)
+                Console.WriteLine(n + " is a prime number.");
+            else
+                Console.WriteLine(n + " is not a prime number.");
+        }
     }
 }
