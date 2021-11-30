de7a1124fefbc0e1625136d940b01f9349a21ef1
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2021-11-30 07:48:51 +0530

    program added to branch1

---
+++ Day6Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using Day6Assignment;
+Console.WriteLine("\t\t((((((Enter y to run and n to stop)))))))");
+char c = Convert.ToChar(Console.ReadLine());
+while (c != 'n')
+{
+    Console.WriteLine("\t\t((((((Please select any one   ))))))");
+    Console.WriteLine("\t\t((((((1.Fibbo Series          ))))))");
+    int Aloo = Convert.ToInt32(Console.ReadLine());
+    switch (Aloo)
+    {
+        case 1:
+            FibbonacciSeries fs = new FibbonacciSeries();
+            fs.Fibbo();
+            break;
+    }
+}
